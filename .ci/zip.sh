79735ea18300f149e7a2b3f390f728dd787d63ca
Margen67 <Margen67@users.noreply.github.com>
2022-04-20 05:27:11 -0700

    Update zip.sh

--- .ci/zip.sh
+++ .ci/zip.sh
@@ -1,11 +1,11 @@
-current=`date '+%Y-%m-%d %H:%M:%S %A (%Z %z)'`
+current=$(date '+%Y-%m-%d %H:%M:%S %A (%Z %z)')
 msg="Patch archive built on: \`$current\`"
-patch_file="patch_built.md"
-cp -r "_patch0" "patch0"
+patch_file=patch_built.md
+cp -r _patch0 patch0
 ls -R _patch0 > manifest_patch0.txt
 find _patch0 -print > manifest_patch1.txt
-echo $msg >> patch0/$patch_file
-echo $msg >> _pages/$patch_file
+echo "$msg" >> patch0/$patch_file
+echo "$msg" >> _pages/$patch_file
 cat patch0/$patch_file
 zip -r _patch/patch.zip patch0
-rm  -r "patch0"
+rm  -r patch0
