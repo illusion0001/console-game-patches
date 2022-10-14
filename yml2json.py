f3e1c52c5180dcf7c0d8e9f2f14cfa6dc42e6488
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-10-13 21:17:03 -0500

    Add missing key for conversion

--- yml2json.py
+++ yml2json.py
@@ -8,6 +8,7 @@ title_str           = 'title'
 app_ver_str         = 'app_ver'
 app_titleid_str     = 'app_titleid'
 app_elf_str         = 'app_elf'
+patch_ver_str       = 'patch_ver'
 name_str            = 'name'
 author_str          = 'author'
 note_str            = 'note'
@@ -38,25 +39,26 @@ for patches in glob.glob('_patch0/orbis/*.yml', recursive=True):
       json_str += ('      "{}": "{}",\n'.format(title_str, patch_file[i].get(title_str,'')))
       json_str += ('      "{}": "{}",\n'.format(app_ver_str, patch_file[i].get(app_ver_str,'')))
       json_str += ('      "{}": "{}",\n'.format(app_elf_str, patch_file[i].get(app_elf_str,'')))
+      json_str += ('      "{}": "{}",\n'.format(patch_ver_str, patch_file[i].get(patch_ver_str,'')))
       json_str += ('      "{}": "{}",\n'.format(name_str, patch_file[i].get(name_str,'')))
       json_str += ('      "{}": "{}",\n'.format(author_str, patch_file[i].get(author_str,'')))
       json_str += ('      "{}": "{}",\n'.format(note_str, replace_all(patch_file[i].get(note_str,''),rlist)))
       json_str += ('      "patch_list": [\n')
       for patch_data in patch_file[i]['patch_list']:
         if (patch_data[0] == 'bytes' or patch_data[0] == 'utf8' or patch_data[0] == 'utf16' ):
-          json_str += ('    { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % (patch_data[0],patch_data[1],replace_all(patch_data[2],rlist))) # hack for json
+          json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % (patch_data[0],patch_data[1],replace_all(patch_data[2],rlist))) # hack for json
         elif (patch_data[0] == 'byte'):
-            json_str += ('    { "type": "%s", "addr": "0x%08x", "value": "0x%02x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
+            json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "0x%02x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
         elif (patch_data[0] == 'bytes16'):
-            json_str += ('    { "type": "%s", "addr": "0x%08x", "value": "0x%04x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
+            json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "0x%04x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
         elif (patch_data[0] == 'bytes32'):
-            json_str += ('    { "type": "%s", "addr": "0x%08x", "value": "0x%08x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
+            json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "0x%08x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
         elif (patch_data[0] == 'bytes64'):
-            json_str += ('    { "type": "%s", "addr": "0x%08x", "value": "0x%016x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
+            json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "0x%016x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
         elif (patch_data[0] == 'float32'):
-            json_str += ('    { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % ('bytes32',patch_data[1],float_to_hex(patch_data[2])))
+            json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % ('bytes32',patch_data[1],float_to_hex(patch_data[2])))
         elif (patch_data[0] == 'float64'):
-            json_str += ('    { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % ('bytes64',patch_data[1],double_to_hex(patch_data[2])))
+            json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % ('bytes64',patch_data[1],double_to_hex(patch_data[2])))
       json_str += '      ]\n'
       json_str += '    },\n'
       for title_ids in patch_file:
