54f1c3f605bf9f7fcc624bcd1c7b45e70dba2a9e
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-11-01 21:30:31 -0500

    Remove float to bytes conversion

--- yml2json.py
+++ yml2json.py
@@ -19,22 +19,18 @@ def replace_all(text, dic):
         text = text.replace(i, j)
     return text
 
-# https://stackoverflow.com/a/23624284
-def float_to_hex(f):
-    return hex(struct.unpack('<I', struct.pack('<f', f))[0])
-
-def double_to_hex(f):
-    return hex(struct.unpack('<Q', struct.pack('<d', f))[0])
-
 rlist = { '\n': '\\n', '\"': '\\"', '\x00' : '' }
 
 for patches in glob.glob('_patch0/orbis/*.yml', recursive=True):
   json_str = ''
+  convert_text_byte = False
   with open(patches, 'r') as file:
     patch_file = yaml.safe_load(file)
     json_str += '{\n'
     json_str += '  "patch": [\n'
     for i in range(0, len(patch_file)):
+      if (patch_file[i].get(title_str,'') == 'Gravity Rush 2 (Gravity Daze 2)'):
+        convert_text_byte = True
       json_str += '    {\n'
       json_str += ('      "{}": "{}",\n'.format(title_str, patch_file[i].get(title_str,'')))
       json_str += ('      "{}": [ '.format(app_titleid_str))
@@ -49,7 +45,8 @@ for patches in glob.glob('_patch0/orbis/*.yml', recursive=True):
       json_str += ('      "{}": "{}",\n'.format(note_str, replace_all(patch_file[i].get(note_str,''),rlist)))
       json_str += ('      "patch_list": [\n')
       for patch_data in patch_file[i]['patch_list']:
-        if (patch_data[0] == 'bytes' or patch_data[0] == 'utf8' or patch_data[0] == 'utf16' ):
+        bytes = ''
+        if (patch_data[0] == 'bytes'):
           json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % (patch_data[0],patch_data[1],replace_all(patch_data[2],rlist))) # hack for json
         elif (patch_data[0] == 'byte'):
             json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "0x%02x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
@@ -59,10 +56,24 @@ for patches in glob.glob('_patch0/orbis/*.yml', recursive=True):
             json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "0x%08x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
         elif (patch_data[0] == 'bytes64'):
             json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "0x%016x" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
-        elif (patch_data[0] == 'float32'):
-            json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % ('bytes32',patch_data[1],float_to_hex(patch_data[2])))
-        elif (patch_data[0] == 'float64'):
-            json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % ('bytes64',patch_data[1],double_to_hex(patch_data[2])))
+        elif (patch_data[0] == 'float32' or patch_data[0] == 'float64'):
+            json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % (patch_data[0],patch_data[1],patch_data[2]))
+        elif (patch_data[0] == 'utf8'):
+            bytes = ''
+            if (convert_text_byte == True):
+              for byte in patch_data[2]:
+                  bytes += byte.encode('utf-8').hex()
+              json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % ('bytes',patch_data[1],bytes))
+            else:
+              json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % (patch_data[0],patch_data[1],replace_all(patch_data[2],rlist)))
+        elif (patch_data[0] == 'utf16'):
+            bytes = ''
+            if (convert_text_byte == True):
+              for byte in patch_data[2]:
+                  bytes += byte.encode('utf-16le').hex()
+              json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % ('bytes',patch_data[1],bytes))
+            else:
+              json_str += ('        { "type": "%s", "addr": "0x%08x", "value": "%s" },\n' % (patch_data[0],patch_data[1],replace_all(patch_data[2],rlist)))
       json_str += '      ]\n'
       json_str += '    },\n'
       file = Path(patches.replace('.yml','.json')).name
